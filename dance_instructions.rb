122afed5b6b11b3fd4b015f1d3c75709f6cf7bac
amulyamari <amulya@saimail.com>
2018-07-24 14:26:33 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,8 @@
+def how_to_be_a_coder
+  puts"step 1: put your hands out"
+  puts"step 2: pretend that you are typing"
+  puts"step 3: move your face closer to your computer"
+  puts"step 4: remember to smile!"
+end
 
-# Code your methods below
+how_to_be_a_coder
